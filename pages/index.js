a33c977ae0c20c08cadc1336d2747e2ea2b9b8cc
Claudwin <claudwin.fortune@gmail.com>
2019-04-12 15:05:48 -0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Index = () => (
+  <div>
+    <Link href="/about">
+      .<a>About Page</a>
+    </Link>
+    <p>Hello Next.js testing testing</p>
+  </div>
+);
+
+export default Index;
